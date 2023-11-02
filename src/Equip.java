d5107603405f740c841b2d57b43d98484a569da7
aRetr0 <gseguraa@edU.tecnocampus.cat>
2023-11-02 09:50:05 +0100

    a

---
+++ src/Equip.java
@@ -0,0 +1,8 @@
+public class Equip {
+    private String nomEquip;
+    private int puntuacio = -1;
+
+    public void setPuntuacio(int novapuntuacio) {
+        this.puntuacio = novapuntuacio;
+    }
+}
