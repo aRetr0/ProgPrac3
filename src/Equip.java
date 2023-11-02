541be32948c5c5dc91f70a2040dbed8e0d99edb2
aRetr0 <gseguraa@edU.tecnocampus.cat>
2023-11-02 10:40:13 +0100

    equpi

--- src/Equip.java
+++ src/Equip.java
@@ -2,7 +2,13 @@ public class Equip {
     private String nomEquip;
     private int puntuacio = -1;
 
-    public void setPuntuacio(int novapuntuacio) {
-        this.puntuacio = novapuntuacio;
-    }
+    public void setPuntuacio(int novapuntuacio) {this.puntuacio = novapuntuacio;}
+
+    public boolean getJugat() {return this.puntuacio != -1;}
+
+    public String toString() {return ("Equip: " + this.nomEquip + " Puntuacio: " + this.puntuacio);}
+
+    public String dades() {return (this.nomEquip + ";" + this.puntuacio);}
+
+    public int compareTo(Equip e) {return Integer.compare(this.puntuacio, e.puntuacio);}
 }
